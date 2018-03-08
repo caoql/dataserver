576e200dd4aecadf44eb931edecb67daad5c7d56
caoql <2397776038@qq.com>
2018-03-08 10:30:13 +0800

    删除多余的文件

--- src/main/java/com/gaode/xtd/admin/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.gaode.xtd.admin.service;
-
-import com.gaode.xtd.admin.domain.query.UserParam;
-import com.gaode.xtd.common.info.ResponseInfo;
-
-public interface UserService {
-	
-	public ResponseInfo queryList(UserParam param);
-}
